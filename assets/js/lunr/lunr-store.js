36a022ba0fe1b98695315c85fabf7643b4af6983
willrkwan <willrkwan@users.noreply.github.com>
2025-09-02 23:18:51 +0000

    Deploy site: 4bb558223239f505fd2596ede1fbd2c761f247f5 4bb558223239f505fd2596ede1fbd2c761f247f5

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Sample Post",
+        "excerpt":"This is a sample post to test pagination and layout.  ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/09/01/sample-post.html",
+        "teaser": null
+      }]
